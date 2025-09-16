2b8e1079ee4ce6af08cb729c375a051a0671d39c
Sebastix <info@sebastix.nl>
2025-09-16 14:59:27 +0200

    tailwindcss and daisyui upgrade

--- web/themes/custom/ccns_theme/tailwind.config.js
+++ web/themes/custom/ccns_theme/tailwind.config.js
@@ -26,21 +26,5 @@ module.exports = {
   ],
   variants: {
     extend: {},
-  },
-  plugins: [
-    require('daisyui')
-  ],
-  daisyui: {
-    themes: [
-      {
-        lofi: {
-          ...require("daisyui/src/theming/themes")["lofi"],
-          info: "FF4D4D",
-          success: "6df78a",
-          warning: "FF4D4D",
-          error: "f64d4d"
-        },
-      },
-    ],
   }
 }
